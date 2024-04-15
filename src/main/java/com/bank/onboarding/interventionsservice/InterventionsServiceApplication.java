402a256078a4bbbf5dae0eadac9644b523aa6090
JoaoBruno1220256 <1220256@isep.ipp.pt>
2024-04-15 20:23:15 +0100

    Added MCS interventions-service

---
+++ src/main/java/com/bank/onboarding/interventionsservice/InterventionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.onboarding.interventionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterventionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterventionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/onboarding/interventionsservice/InterventionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.onboarding.interventionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterventionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
